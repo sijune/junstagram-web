1b2d5ca9006b01bfab21775bace762b838149970
sijune <sijune0525@gmail.com>
2021-08-07 15:22:03 +0900

    junstagram-web

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -2,17 +2,16 @@ import { Helmet } from "react-helmet-async";
 import PropTypes from "prop-types";
 
 function PageTitle({ title }) {
-    return ( <
-        Helmet >
-        <
-        title > { title } | Junstagram < /title> <
-        /Helmet>
-    );
+  return (
+    <Helmet>
+      <title> {title} | Junstagram </title>
+    </Helmet>
+  );
 }
 
 // prop 체크
 PageTitle.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default PageTitle;
--- src/components/auth/Button.js
+++ src/components/auth/Button.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Button = styled.input `
   border: none;
+  border-radius: 3px;
   margin-top: 12px;
   background-color: ${(props) => props.theme.accent};
   color: white;
